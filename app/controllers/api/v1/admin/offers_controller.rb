a3959304156138330beef7013a31c9e4c3006b14
Regis Seki <regisseki@gmail.com>
2020-07-03 19:09:04 -0300

    Added controller offers functions

--- app/controllers/api/v1/admin/offers_controller.rb
+++ app/controllers/api/v1/admin/offers_controller.rb
@@ -4,19 +4,35 @@ module Api
     module Admin
       class OffersController < ApplicationController
         def show
-          binding.pry
         end
 
         def create
-          binding.pry
+          offer = Offer.new(offer_params)
+          if offer.save
+            render json: {status: 'SUCCESS', message:'Offer Saved', data:offer},status: :ok
+          else
+            render json: {status: 'ERROR', message:'Offer not saved', data:offer.errors},status: :unprocessable_entity
+          end
         end
 
         def update
-          binding.pry
+          offer = Offer.find(params[:id])
+          if offer.update(offer_params)
+            render json: {status: 'SUCCESS', message:'Offer Updated', data:offer},status: :ok
+          else
+            render json: {status: 'ERROR', message:'Offer not updated', data:offer.errors},status: :unprocessable_entity
+          end
         end
 
         def destroy
-          binding.pry
+          offer = Offer.find(params[:id])
+          offer.destroy
+          render json: {status: 'SUCCESS', message:'Offer deleted', data:offer},status: :ok
+        end
+
+        private
+        def offer_params
+          params.permit(:advertiser_id, :url, :description, :starts_at, :ends_at, :premium)
         end
       end
     end
