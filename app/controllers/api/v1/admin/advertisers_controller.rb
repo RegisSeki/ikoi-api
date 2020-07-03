3cafdf9b3fdf30c5e9d0fc1fb661cd9e2bb8ea99
Regis Seki <regisseki@gmail.com>
2020-07-03 19:04:28 -0300

    Fix TYPO

--- app/controllers/api/v1/admin/advertisers_controller.rb
+++ app/controllers/api/v1/admin/advertisers_controller.rb
@@ -18,16 +18,16 @@ module Api
           if advertiser.save
             render json: {status: 'SUCCESS', message:'Saved advertiser', data:advertiser},status: :ok
           else
-            render json: {status: 'ERROR', message:'Advertiser not saved', data:advertiser.erros},status: :unprocessable_entity
+            render json: {status: 'ERROR', message:'Advertiser not saved', data:advertiser.errors},status: :unprocessable_entity
           end
         end
 
         def update
           advertiser = Advertiser.find(params[:id])
-          if advertiser.update_attributes(advertiser_params)
+          if advertiser.update(advertiser_params)
             render json: {status: 'SUCCESS', message:'Updated advertiser', data:advertiser},status: :ok
           else
-            render json: {status: 'ERROR', message:'Advertiser not update', data:advertiser.erros},status: :unprocessable_entity
+            render json: {status: 'ERROR', message:'Advertiser not update', data:advertiser.errors},status: :unprocessable_entity
           end
         end
 
