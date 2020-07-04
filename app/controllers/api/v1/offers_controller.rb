460e648db3839c01e1ac82ab883a31081414ccf7
Regis Seki <regisseki@gmail.com>
2020-07-04 12:52:13 -0300

    Added offer list for users endpoint and some validations

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class OffersController < ApplicationController
       def index
+        offers = Offer.enabled_offers
+        render json: {status: 'SUCCESS', message:'Offer Updated', data:offers},status: :ok
       end
     end
   end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,10 +2,15 @@ class Offer < ApplicationRecord
   belongs_to :advertiser
 
   validates :url, presence: true
-  validates :description, presence: true
+  validates_format_of :url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
+  validates :description, presence: true, length: { maximum: 500 }
   validates :starts_at, presence: true
 
   def self.by_advertiser_id(advertiser_id)
     where(advertiser_id: advertiser_id)
+  end
+
+  def self.enabled_offers
+    where("starts_at <= ? AND (ends_at >= ? OR ends_at IS NULL)", Time.now, Time.now).order("premium = true DESC")
   end
 end
