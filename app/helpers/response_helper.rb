c862ab75e3a3be6613ea87bd91f8ea09e1807bd7
Regis Seki <regisseki@gmail.com>
2020-07-04 10:27:42 -0300

    Removed unecessary class

--- app/helpers/response_helper.rb
+++
@@ -1,9 +0,0 @@
-module ResponseHelper
-  def json_response_ok(message, data)
-    json: {status: 'SUCCESS', message:message, data:data},status: :ok
-  end
-
-  def json_response_error(message, data)
-    json: {status: 'ERROR', message:message, data:data.errors},status: :unprocessable_entity
-  end
-end
