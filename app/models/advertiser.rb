e7e3ac5789903741730ad551c712bd88bd5470f1
Regis Seki <regisseki@gmail.com>
2020-07-04 12:47:07 -0300

    Added url validation and uniquiness for thecolumn name

--- app/models/advertiser.rb
+++ app/models/advertiser.rb
@@ -2,7 +2,9 @@ class Advertiser < ApplicationRecord
   has_many :offers
 
   validates :name, presence: true
+  validates_uniqueness_of :name
   validates :url, presence: true
+  validates_format_of :url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
 
   def self.by_id(id)
     where(:id)
